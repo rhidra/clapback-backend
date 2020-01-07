39a5c06a9137bf87bb4a177bc9797a3810a70471
Rémy Hidra <remy.hidra@gmail.com>
2020-01-07 23:06:27 +0800

    Add a route for the latest news group

--- api/src/middleware/utils.ts
+++ api/src/middleware/utils.ts
@@ -8,9 +8,18 @@ export function handleError(err: mongoose.Error, res: express.Response) {
     }
 }
 
-export function sendData(res: express.Response) {
+export function sendData_cb(res: express.Response, cb= (a: any) => a) {
     return (err: mongoose.Error, data: any) => {
         this.handleError(err, res);
+        data = cb(data);
         res.send(data);
     };
+}
+
+export function sendData(res: express.Response, err: mongoose.Error, data: any): Promise<any> {
+    return new Promise<any>(resolve => {
+        this.handleError(err, res);
+        res.send(data);
+        resolve();
+    });
 }
--- api/src/routes/news.ts
+++ api/src/routes/news.ts
@@ -2,23 +2,39 @@ import * as express from 'express';
 import mongoose from 'mongoose';
 import NewsItem from '../models/NewsItemModel';
 import NewsGroup from '../models/NewsGroupModel';
-import {handleError, sendData} from '../middleware/utils';
+import moment from 'moment';
+import {handleError, sendData, sendData_cb} from '../middleware/utils';
 
 const db = mongoose.connection;
 const router = express.Router();
 
+/** /news/latest
+ * Send the latest news group that is not in the future.
+ */
+router.route('/latest')
+    .get((req, res) => NewsGroup.find({})
+        .then(data => new Promise(resolve => {
+            const isLatest = (ltst: any, grp: any): boolean =>
+                moment(grp.date) > moment(ltst.date) && moment(grp.date) <= moment();
+            const l = data.reduce((latest: any, group: any) => isLatest(latest, group) ? group : latest);
+            resolve(l);
+        }))
+        .catch(err => handleError(err, res))
+        .then((group: any) => res.redirect('/news/group/' + group.id))
+    );
+
 router.route('/item')
-    .get((req, res) => NewsItem.find({}, sendData(res)))
-    .post((req, res) => NewsItem.create(req.body, sendData(res)));
+    .get((req, res) => NewsItem.find({}, sendData_cb(res)))
+    .post((req, res) => NewsItem.create(req.body, sendData_cb(res)));
 
 router.route('/item/:id')
-    .get((req, res) => NewsItem.findById(req.params.id, sendData(res)))
-    .post((req, res) => NewsItem.findOneAndUpdate({_id: req.params.id}, req.body, {}, sendData(res)))
-    .delete((req, res) => NewsItem.findOneAndDelete({_id: req.params.id}, sendData(res)));
+    .get((req, res) => NewsItem.findById(req.params.id, sendData_cb(res)))
+    .post((req, res) => NewsItem.findOneAndUpdate({_id: req.params.id}, req.body, {}, sendData_cb(res)))
+    .delete((req, res) => NewsItem.findOneAndDelete({_id: req.params.id}, sendData_cb(res)));
 
 router.route('/group')
-    .get((req, res) => NewsGroup.find({}, sendData(res)))
-    .post((req, res) => NewsGroup.create(req.body, sendData(res)));
+    .get((req, res) => NewsGroup.find({}, sendData_cb(res)))
+    .post((req, res) => NewsGroup.create(req.body, sendData_cb(res)));
 
 router.route('/group/:id')
     .get((req, res) => NewsGroup.findById(req.params.id, (err, group: any) => {
@@ -28,7 +44,7 @@ router.route('/group/:id')
             res.send(Object.assign({items}, group._doc));
         });
     }))
-    .post((req, res) => NewsGroup.findOneAndUpdate({_id: req.params.id}, req.body, {}, sendData(res)))
-    .delete((req, res) => NewsGroup.findOneAndDelete({_id: req.params.id}, sendData(res)));
+    .post((req, res) => NewsGroup.findOneAndUpdate({_id: req.params.id}, req.body, {}, sendData_cb(res)))
+    .delete((req, res) => NewsGroup.findOneAndDelete({_id: req.params.id}, sendData_cb(res)));
 
 export = router;
