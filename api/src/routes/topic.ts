703a31a1231e218cae97a45124bbc3c1e821cfc8
Rémy Hidra <remy.hidra@gmail.com>
2020-02-28 21:41:08 +0800

    Fix get all topic route

--- api/src/routes/topic.ts
+++ api/src/routes/topic.ts
@@ -11,9 +11,8 @@ const guard = express_jwt_permissions();
 
 router.route('/')
   // Retrieve news topics
-  .get(notAuth, (req, res) =>
-    Topic.find({approved: req.query.approved || !req.user || !(req.user as any).permissions.includes('creator')},
-      sendData_cb(res)).sort({date: 'asc'}))
+  .get(notAuth, (req, res) => Topic.find(req.query.approved || !req.user || !(req.user as any).permissions.includes('creator') ?
+      {approved: true} : {}, sendData_cb(res)).sort({date: 'asc'}))
 
   // Create a news topic
   .post(auth, guard.check('creator'), (req, res) => Topic.create(req.body).then((topic: any) => {
