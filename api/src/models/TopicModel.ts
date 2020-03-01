0a692d79d812acfc4dc7ad9190b865c9e88cdfdb
Rémy Hidra <remy.hidra@gmail.com>
2020-03-01 18:57:56 +0800

    Add a center panel field and authors to each panel

--- api/src/models/TopicModel.ts
+++ api/src/models/TopicModel.ts
@@ -5,17 +5,21 @@ const Schema = mongoose.Schema;
 const TopicSchema = new Schema({
     title: String,
 
-    author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    video: { type: String, required: true },
     hashtag: { type: String, required: true },
     approved: { type: Boolean, default: false },
     date: { type: Date, required: true },
+
+    centerPanel: {
+        video: { type: String, required: true },
+        author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    },
 
     leftPanel: {
         video: String,
         text: String,
         image: String,
         quiz: { type: Schema.Types.ObjectId, ref: 'Quiz' },
+        author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     },
 
     rightPanel: {
@@ -23,6 +27,7 @@ const TopicSchema = new Schema({
         text: String,
         image: String,
         quiz: { type: Schema.Types.ObjectId, ref: 'Quiz' },
+        author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     },
 });
 
