89ab1c4ac3709d4c2980cde5cda4122d2079d226
Rémy Hidra <remy.hidra@gmail.com>
2020-03-14 03:04:55 +0800

    Fix return error log

--- api/src/middleware/utils.ts
+++ api/src/middleware/utils.ts
@@ -8,21 +8,24 @@ export function handleError(err: mongoose.Error | any, res: express.Response, er
         res.status(errorCode);
         res.send(err);
     }
+    return !!err;
 }
 
 export function sendData_cb(res: express.Response, cb = (a: any) => a) {
     return (err: mongoose.Error, data: any = {}) => {
-        this.handleError(err, res);
-        data = cb(data);
-        res.send(data);
+        if (!this.handleError(err, res)) {
+            data = cb(data);
+            res.send(data);
+        }
     };
 }
 
 export function sendData(res: express.Response, err: mongoose.Error, data: any): Promise<any> {
     return new Promise<any>(resolve => {
-        this.handleError(err, res);
-        res.send(data);
-        resolve();
+        if (!this.handleError(err, res)) {
+            res.send(data);
+            resolve();
+        }
     });
 }
 
