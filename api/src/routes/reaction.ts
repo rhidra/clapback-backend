693225d821d1b8d56077ad7c999b0e57af3a29f5
Rémy Hidra <remy.hidra@gmail.com>
2020-03-17 16:33:37 +0100

    Add restrictions for reactions

--- api/src/routes/reaction.ts
+++ api/src/routes/reaction.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
 import mongoose from 'mongoose';
-import {sendData, sendData_cb} from '../middleware/utils';
+import {sendData, sendData_cb, sendError} from '../middleware/utils';
 import Reaction from '../models/ReactionModel';
 import jwt from 'express-jwt';
 import express_jwt_permissions from 'express-jwt-permissions';
@@ -15,22 +15,33 @@ router.route('/')
 /**
  * GET /reaction
  * Send all reaction
- * @param populate
+ * @param type (mandatory) "video" for video-text reaction or "text" for text-only
+ * @param populate (optionnal)
  */
-  .get(auth, guard.check('user'), (req, res) => Reaction.find({})
-    .then(docs => {
-      if (req.query.populate && req.query.populate === 'true') {
-        return Promise.all(docs.map(e => e.populate('user').populate('topic').execPopulate()));
-      }
-      return Promise.resolve(docs);
-    })
-    .then(docs => sendData(res, null, docs)))
+  .get(auth, guard.check('user'), (req, res) => {
+    if (req.query.type !== 'video' && req.query.type !== 'text') { return sendError('Type wrong !', res, 400); }
 
-  /**
-   *  POST /reaction
-   * Create a quiz
-   */
-  .post(auth, guard.check('user'), (req, res) => Reaction.create(req.body, sendData_cb(res)));
+    Reaction.find(req.query.type === 'video' ? {$and: [{video: {$ne: null}}, {video: {$ne: ''}}]}
+                                                        : {$or: [{video: null}, {video: ''}]})
+      .then(docs => {
+        if (req.query.populate && req.query.populate === 'true') {
+          return Promise.all(docs.map(e => e.populate('user').populate('topic').execPopulate()));
+        }
+        return Promise.resolve(docs);
+      })
+      .then(docs => sendData(res, null, docs));
+  })
+
+/**
+ * POST /reaction
+ * Create a quiz. Allowed to admins.
+ */
+  .post(auth, guard.check('user'), (req, res) => {
+    if (!(req.user as any).permissions.includes('admin') && (req.user as any)._id !== req.body.user) {
+      return sendError('Wrong user !', res);
+    }
+    Reaction.create(req.body, sendData_cb(res));
+  });
 
 router.route('/:id')
 /**
@@ -40,12 +51,32 @@ router.route('/:id')
   .get((req, res) => Reaction.findById(req.params.id).populate('user').populate('topic').exec(sendData_cb(res)))
 
 /**
- * POST /quiz/:id
- * Modify a quiz.
+ * POST /reaction/:id
+ * Modify a quiz. Allowed to editors.
  */
-  .post(auth, guard.check('user'), (req, res) =>
-    Reaction.findOneAndUpdate({_id: req.params.id}, req.body, {}, sendData_cb(res)))
-  .delete(auth, guard.check('user'), (req, res) => Reaction
-    .findOneAndDelete({_id: req.params.id}, sendData_cb(res)));
+  .post(auth, guard.check('user'), (req, res) => {
+    if (!(req.user as any).permissions.includes('editor') && (req.user as any)._id !== req.body.user) {return sendError('Wrong user !', res, 403); }
+    Reaction.findById(req.params.id).then((reaction: any) => {
+      if (!reaction) {
+        return sendError('Reaction does not exist !', res, 400);
+      } else if (!(req.user as any).permissions.includes('editor') && (req.user as any)._id !== reaction.user) {
+        return sendError('Wrong user !', res, 403);
+      } else {
+        Object.assign(reaction, req.body);
+        reaction.save().then(sendData_cb(res));
+      }
+    });
+  })
+
+/**
+ * DELETE /reaction/:id
+ * Delete a reaction. Allowed to editors.
+ */
+  .delete(auth, guard.check('user'), (req, res) =>  {
+    if (!(req.user as any).permissions.includes('editor') && (req.user as any)._id !== req.body.user) {
+      return sendError('Wrong user !', res, 403);
+    }
+    Reaction.findOneAndDelete({_id: req.params.id}, sendData_cb(res));
+  });
 
 export = router;
