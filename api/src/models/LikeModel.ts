b9142e2f5a6a0ea33f13034e57f81c6fabf24fcd
Rémy Hidra <remy.hidra@gmail.com>
2020-03-19 13:16:55 +0100

    Add a basic reaction like system

--- api/src/models/LikeModel.ts
+++ api/src/models/LikeModel.ts
@@ -3,11 +3,14 @@ import * as mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 /**
- * Represents the like of a user to a news topic.
+ * Represents the like of a user to a topic, a reaction or a comment.
  */
 const LikeSchema = new Schema({
-    topic: { type: Schema.Types.ObjectId, ref: 'Topic' },
-    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+
+    topic: { type: Schema.Types.ObjectId, ref: 'Topic', required: false },
+    reaction: { type: Schema.Types.ObjectId, ref: 'Reaction', required: false },
+    comment: { type: Schema.Types.ObjectId, ref: 'Comment', required: false },
 });
 
 export = mongoose.model('Like', LikeSchema);
--- api/src/routes/like.ts
+++ api/src/routes/like.ts
@@ -13,7 +13,7 @@ const guard = express_jwt_permissions();
  * /like/topic
  */
 router.route('/topic')
-  // Check if a like to a topic exists
+// Check if a like to a topic exists
   .get(auth, guard.check('user'), (req, res) => Like
     .findOne({topic: req.query.topic, user: (req.user as any)._id}, sendData_cb(res)))
 
@@ -32,41 +32,27 @@ router.route('/topic')
     .deleteOne({topic: req.body.topic, user: (req.user as any)._id}, sendData_cb(res))
   );
 
-/*
-router.route('/:id')
 /**
- * GET /topic/:id
- * Retrieve a specific news topic
- * @param populate Populate the author field in each panel
- *
-  .get(notAuth, (req, res) => Topic.findById(req.params.id)
-    .then(topic =>  {
-      if (req.query.populate && req.query.populate === 'true') {
-        return topic.populate('centerPanel.author')
-          .populate('leftPanel.author').populate('rightPanel.author').execPopulate();
+ * /like/reaction
+ */
+router.route('/reaction')
+// Check if a like to a reaction exists
+  .get(auth, guard.check('user'), (req, res) => Like
+    .findOne({reaction: req.query.reaction, user: (req.user as any)._id}, sendData_cb(res)))
+
+  // Add a like to a reaction
+  .post(auth, guard.check('user'), (req, res) => Like
+    .findOne({reaction: req.body.reaction, user: (req.user as any)._id}).then(duplicate => {
+      if (!duplicate) {
+        return Like.create({reaction: req.body.reaction, user: (req.user as any)._id}, sendData_cb(res));
       }
-      return new Promise(r => r(topic));
-    })
-    .then((topic: any) => {
-      if ((!req.user || !(req.user as any).permissions.includes('creator')) && !topic.approved) {
-        return sendError('Unauthorized', res, 403);
-      }
-      return sendData(res, null, topic);
+      return sendSuccess(res);
     })
   )
 
-  // Modify a specific news topic
-  .post(auth, guard.check('creator'),
-    (req, res) => Topic.findOneAndUpdate({_id: req.params.id}, req.body).then((topic: any) => {
-      if (!(req.user as any).permissions.includes('editor')) {
-        topic.approved = false;
-        topic.save();
-      }
-      return sendData(res, null, topic);
-    }).catch(err => sendError(err, res)))
-
-  // Delete a specific news topic
-  .delete(auth, guard.check('editor'), (req, res) => Topic.findOneAndDelete({_id: req.params.id}, sendData_cb(res)));
-*/
+  // Remove a like to a reaction
+  .delete(auth, guard.check('user'), (req, res) => Like
+    .deleteOne({reaction: req.body.reaction, user: (req.user as any)._id}, sendData_cb(res))
+  );
 
 export = router;
