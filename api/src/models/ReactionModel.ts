400f6a95abcc552c11fe245287ba297a776ac4dd
Rémy Hidra <remy.hidra@gmail.com>
2020-03-20 19:21:34 +0100

    Add a clapback counter

--- api/src/models/ReactionModel.ts
+++ api/src/models/ReactionModel.ts
@@ -1,4 +1,6 @@
 import * as mongoose from 'mongoose';
+import Topic from './TopicModel';
+import {handleError} from '../middleware/utils';
 
 const Schema = mongoose.Schema;
 
@@ -8,8 +10,8 @@ const Schema = mongoose.Schema;
  * If it is a text, it is just a comment.
  */
 const ReactionSchema = new Schema({
-    topic: { type: Schema.Types.ObjectId, ref: 'Topic' },
-    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    topic: { type: Schema.Types.ObjectId, ref: 'Topic', required: true },
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     date: { type: Date, required: true, default: Date.now },
 
     video: String,
@@ -18,6 +20,18 @@ const ReactionSchema = new Schema({
     likesCounter: { type: Number, default: 0 },
     commentsCounter: { type: Number, default: 0 },
     viewsCounter: { type: Number, default: 0 },
+});
+
+ReactionSchema.post('save', (doc: any) => {
+    if (doc.topic) {
+        Topic.findByIdAndUpdate(doc.topic, {$inc: {clapbacksCounter: 1}}, err => handleError(err));
+    }
+});
+
+ReactionSchema.post('remove', (doc: any) => {
+    if (doc.topic) {
+        Topic.findByIdAndUpdate(doc.topic, {$inc: {clapbacksCounter: -1}}, err => handleError(err));
+    }
 });
 
 export = mongoose.model('Reaction', ReactionSchema);
