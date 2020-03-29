c3c5e32990831e7498c0c84d3f488fa2b96801a1
Rémy Hidra <remy.hidra@gmail.com>
2020-03-29 16:24:41 +0200

    Add a get results route

--- api/src/routes/vote.ts
+++ api/src/routes/vote.ts
@@ -1,7 +1,8 @@
 import * as express from 'express';
 import mongoose from 'mongoose';
-import {sendData_cb, sendError, sendSuccess} from '../middleware/utils';
+import {sendData, sendData_cb, sendError, sendSuccess} from '../middleware/utils';
 import Vote from '../models/VoteModel';
+import Quiz from '../models/QuizModel';
 import jwt from 'express-jwt';
 import express_jwt_permissions from 'express-jwt-permissions';
 
@@ -41,5 +42,18 @@ router.route('/:idQuiz')
   .delete(auth, guard.check('user'), (req, res) => Vote
     .deleteMany({quiz: req.params.idQuiz, user: (req.user as any)._id}, sendData_cb(res))
   );
+
+/**
+ * GET /quiz/vote/:idQuiz/results
+ * Retrieve the results of the quiz, with the format [{choice, count}].
+ */
+router.get('/:idQuiz/results', auth, guard.check('user'), (req, res) => Quiz.findById(req.params.idQuiz)
+  .then((quiz: any) => Promise.all(quiz.choices
+    .map((choice: any) => Vote
+      .count({quiz: quiz._id, choice: choice._id}).exec()))
+    .then((counts: Array<number|any>) =>
+      sendData(res, null, counts.map((count, i) => ({choice: quiz.choices[i]._id, count}))))
+  ).catch(err => sendError(err, res, 400))
+);
 
 export = router;
