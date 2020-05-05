96488c45afac5706cdd2f2f0d3e15e9f55a65c3d
Rémy Hidra <remy.hidra@gmail.com>
2020-05-05 12:26:01 +0200

    Fix mongo connection error

--- src/app.ts
+++ src/app.ts
@@ -25,15 +25,15 @@ import * as Sentry from '@sentry/node';
 const port = process.env.PORT || 80;
 const app = express();
 Sentry.init({
-    dsn: 'https://6cae9a572c7748bbae71b7218abc4df0@o373953.ingest.sentry.io/5191229',
-    environment: process.env.NODE_ENV
+  dsn: 'https://6cae9a572c7748bbae71b7218abc4df0@o373953.ingest.sentry.io/5191229',
+  environment: process.env.NODE_ENV
 });
 
 // Sentry request handler
 app.use(Sentry.Handlers.requestHandler() as express.RequestHandler);
 
 app.use(logger(process.env.NODE_ENV === 'development' ? 'dev' : 'combined', {
-    skip: req => req.originalUrl === '/health' || req.originalUrl === '/'
+  skip: req => req.originalUrl === '/health' || req.originalUrl === '/'
 }));
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -42,10 +42,20 @@ app.use(cors());
 app.set('view engine', 'ejs');
 
 // Connecting to MongoDB
-mongoose.connect(process.env.MONGODB_URI).then(() => {
-    mongoose.connection.on('error', (err: any) => console.log('Database connection error:', err));
-    mongoose.connection.once('open', () => console.log('Connected to Database!'));
-}, () => console.log('Not connected to database !'));
+mongoose.connection.on('connecting', () => console.log('connecting to MongoDB...'));
+mongoose.connection.on('error', (error) => {
+  console.error('Error in MongoDb connection: ' + error);
+  console.error('Mongo URI: ' + process.env.MONGODB_URI);
+  mongoose.disconnect();
+});
+mongoose.connection.on('connected', () => console.log('MongoDB connected!'));
+mongoose.connection.once('open', () => console.log('MongoDB connection opened!'));
+mongoose.connection.on('reconnected', () => console.log('MongoDB reconnected!'));
+mongoose.connection.on('disconnected', () => {
+  console.log('MongoDB disconnected!');
+  mongoose.connect(process.env.MONGODB_URI, {server: {auto_reconnect: true}});
+});
+mongoose.connect(process.env.MONGODB_URI, {server: {auto_reconnect: true}});
 
 app.use(auth.initialize());
 
@@ -65,9 +75,9 @@ app.get('/health', (req, res) => res.send());
 
 // Sentry error handler
 app.use(Sentry.Handlers.errorHandler({
-    shouldHandleError(error): boolean {
-        return true; // TODO: Filter error by status (e.g. error.status === 404)
-    }
+  shouldHandleError(error): boolean {
+    return true; // TODO: Filter error by status (e.g. error.status === 404)
+  }
 }) as express.ErrorRequestHandler);
 
 // Catch 404 and forward to error handler
