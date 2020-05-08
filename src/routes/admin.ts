2a6f7e99291f9a06dd25ba7e109d0bea5b1f0df6
Rémy Hidra <remy.hidra@gmail.com>
2020-05-08 19:43:46 +0200

    Add route for server storage infos

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -3,12 +3,30 @@ import jwt from 'express-jwt';
 import express_jwt_permissions from 'express-jwt-permissions';
 import {Router} from 'express';
 import * as path from 'path';
-import {sendSuccess} from '../middleware/utils';
+import {sendError, sendSuccess} from '../middleware/utils';
+import {exec} from 'child_process';
 
 const router = Router();
 const auth = jwt({secret: process.env.JWT_SECRET});
 const guard = express_jwt_permissions();
 const cwd: string = process.cwd();
+
+/**
+ * Executes a shell command and return it as a Promise.
+ * @param cmd {string}
+ * @return {Promise<string>}
+ */
+function execCmd(cmd: string) {
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+        reject(stderr);
+      } else {
+        resolve(stdout);
+      }
+    });
+  });
+}
 
 /*** ROUTES ONLY FOR ADMINS ! ***/
 router.use(auth, guard.check('admin'));
@@ -27,6 +45,27 @@ router.get('/media', (req, res) => {
     .reduce((s: number, media: string) => s + fs.statSync(path.join(cwd, 'public/media', media)).size, 0);
 
   res.json({medias, thumbnailsSize, mediasSize});
+});
+
+/**
+ * GET /admin/storage
+ * Only for admins.
+ * Monitor storage space on the server.
+ * @return {total, used} in Gigabytes
+ */
+router.get('/storage', (req, res) => {
+  const dir = process.env.NODE_ENV === 'development' ? '/dev/sda5' : '/dev/vda1';
+  const totalCmd = 'df -k --output=size ' + dir;
+  const usedCmd = 'df -k --output=used ' + dir;
+
+  Promise
+    .all([execCmd(totalCmd), execCmd(usedCmd)])
+    .then((s: any) => {
+      const total = Number(s[0].split('\n')[1].trim()) * 1024 / 1073741824;
+      const used = Number(s[1].split('\n')[1].trim()) * 1024 / 1073741824;
+      res.json({total, used});
+    })
+    .catch(err => sendError(err, res));
 });
 
 /**
