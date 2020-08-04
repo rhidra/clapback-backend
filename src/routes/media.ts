2a3cdd154cc6da6a8b6c5e8b1ca6176371c31465
Rémy Hidra <remy.hidra@gmail.com>
2020-08-04 19:40:31 +0200

    Convert and save mp4 video to a HLS stream playlist in sync

--- src/routes/media.ts
+++ src/routes/media.ts
@@ -11,6 +11,7 @@ import {Router, Request} from 'express';
 const genThumbnail = require('simple-thumbnail');
 // tslint:disable-next-line:no-var-requires
 const ffmpeg = require('ffmpeg-static');
+import {spawn, exec} from 'child_process';
 
 const router = Router();
 const auth = jwt({secret: process.env.JWT_SECRET});
@@ -73,20 +74,67 @@ function modifyImage(image: any, filename: string, opt: ImageOptions, out?: stri
  * Delete a file on the disk
  */
 
-router.post('/', auth, upload.single('media'), (req, res) => {
+router.post('/', auth, upload.single('media'), async (req, res) => {
   if (!req.file) {
     sendError('No media received !', res);
   }
   const ext = getExtension(req.file.originalname);
   const [opt, _] = extractOptions(req);
-  const filename = `${uuidv4()}` + '.' + ext;
+  const fileId = `${uuidv4()}`;
+  const filename = fileId + '.' + ext;
 
   if (supportedImages.includes(ext)) {
-    modifyImage(req.file.buffer, filename, opt)
-      .then(() => res.send({filename: buildUrl(filename)}));
+    await modifyImage(req.file.buffer, filename, opt);
+    await res.send({filename: buildUrl(filename)});
   } else if (supportedVideos.includes(ext)) {
-    fs.writeFileSync(buildPath(filename), req.file.buffer);
+    const fileMP4Path = 'public/mp4/' + filename;
+    fs.writeFileSync(fileMP4Path, req.file.buffer);
     res.send({filename: buildUrl(filename)});
+
+    // Video encoding in HLS for adaptive bitrate and resolution streaming
+    // Reference : https://www.martin-riedl.de/2020/04/17/using-ffmpeg-as-a-hls-streaming-server-overview/
+    const hlsPath = path.join(process.cwd(), `public/hls/${fileId}`);
+    fs.mkdirSync(hlsPath, {recursive: true});
+    const ffmpegExec = path.join(path.dirname(require.resolve('ffmpeg-static')), 'ffmpeg');
+    console.log(path.join(process.cwd(), `public/hls/${fileId}`));
+    const child = spawn(ffmpegExec, [
+      '-i', `${path.join(process.cwd(), fileMP4Path)}`,
+      // Creates two video feed, down scaling the resolution
+      '-filter_complex',
+      '[v:0]split=2[vtemp001][vtemp002];[vtemp001]scale=w=234:h=416[vout001];[vtemp002]scale=w=720:h=1280[vout002]',
+      // Speed of conversion, fix framerate at 25, fix the segment duration
+      '-preset', 'veryfast', '-g', '25', '-sc_threshold', '0',
+
+      // Creates 2 versions: codec H.264, 2000k/6000k bitrate, with a bitrate cap at 10% and some buffer size
+      '-map', '[vout001]', '-c:v:0', 'libx264', '-b:v:0',  '145k', '-maxrate:v:0',  '160k', '-bufsize:v:0',  '800k',
+      '-map', '[vout002]', '-c:v:1', 'libx264', '-b:v:1', '3000k', '-maxrate:v:1', '3300k', '-bufsize:v:1', '4000k',
+
+      // For the audio, the two feed are identical: Audio encoding (AAC), audio bitrate, stereo
+      '-map', 'a:0', '-map', 'a:0',
+      '-c:a', 'aac', '-b:a', '128k', '-ac', '2',
+
+      // Output format HLS, 6 seconds videos
+      '-f', 'hls', '-hls_time', '6', '-hls_playlist_type', 'event', '-hls_flags', 'independent_segments',
+
+      // File structure settings
+      '-master_pl_name', 'master.m3u8',
+      '-hls_segment_filename', `stream_%v/data_%06d.ts`, '-strftime_mkdir', '1',
+      '-var_stream_map', 'v:0,a:0 v:1,a:1',
+
+      // Output
+      `stream_%v.m3u8`,
+    ], {cwd: hlsPath});
+
+    child.stdout.pipe(process.stdout);
+    child.stderr.pipe(process.stderr);
+    child.once('error', (err: Error) => console.log(`Error during the conversion: ${err}`));
+    child.once('exit', (code: number, signal: string) => {
+      if (code === 0) {
+        console.log(`Convert video file ${fileId} with success !`);
+      } else {
+        console.log(`Bad error code at the exit of the conversion for video ${fileId} !`);
+      }
+    });
   } else {
     sendError('File format unsupported !', res);
   }
