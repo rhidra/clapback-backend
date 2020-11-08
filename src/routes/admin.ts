2cb2e5507a773b6337a82abe19e6aab851923802
Rémy Hidra <remy.hidra@gmail.com>
2020-11-08 17:06:56 +0100

    Add handling of modified images

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -12,6 +12,7 @@ const router = Router();
 const auth = jwt({secret: process.env.JWT_SECRET});
 const guard = express_jwt_permissions();
 const exec = util.promisify(exec_);
+const unlink = util.promisify(fs.unlink);
 const cwd: string = process.cwd();
 
 /*** ROUTES ONLY FOR ADMINS ! ***/
@@ -40,16 +41,19 @@ router.get('/media/videos', async (req, res) => {
 /**
  * GET /admin/media/images
  * Only for admins.
- * Return infos about images stored on the server.
+ * Return infos about images and modified images stored on the server.
+ * The size of all images contains the size of all modified images.
  * All size units are in bytes.
- * @return {images: [string], size: number}
+ * @return {images: [string], size: number, modifiedSize: number}
  */
 router.get('/media/images', (req, res) => {
   const images: string[] = fs.readdirSync(path.join(cwd, 'public/image'));
   const size = images
     .reduce((s: number, file: string) => s + fs.statSync(path.join(cwd, 'public/image', file)).size, 0);
+  const modifiedSize = images.filter(s => s.includes('_'))
+    .reduce((s: number, file: string) => s + fs.statSync(path.join(cwd, 'public/image', file)).size, 0);
 
-  return sendData(res, null, {images, size});
+  return sendData(res, null, {images, size, modifiedSize});
 });
 
 /**
@@ -69,12 +73,21 @@ router.get('/storage', (req, res) => {
  * Only for admins.
  * Remove all the thumbnail cache from the disk.
  */
-router.delete('/thumbnails', (req, res) => {
-  Promise
-    .all(fs.readdirSync(path.join(cwd, 'public/thumbnail')).map(tb => new Promise(resolve =>
-      fs.unlink(path.join(cwd, 'public/thumbnail', tb), () => resolve())
-    )))
-    .then(() => sendSuccess(res));
+router.delete('/thumbnails', async (req, res) => {
+  const thumbnails = fs.readdirSync(path.join(cwd, 'public/thumbnail'));
+  await Promise.all(thumbnails.map(tb => unlink(path.join(cwd, 'public/thumbnail', tb))));
+  sendSuccess(res);
+});
+
+/**
+ * DELETE /admin/modified-images
+ * Only for admins.
+ * Remove all the modified images from the disk.
+ */
+router.delete('/modified-images', async (req, res) => {
+  const modifiedImages = fs.readdirSync(path.join(cwd, 'public/image')).filter(s => s.includes('_'));
+  await Promise.all(modifiedImages.map(img => unlink(path.join(cwd, 'public/image', img))));
+  sendSuccess(res);
 });
 
 /**
